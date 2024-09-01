a190f84396812e2cc49663b059d85d379a3e136c
CodeRood47 <145922302+CodeRood47@users.noreply.github.com>
2024-09-01 17:21:29 +0200

    Initial commit

---
+++ Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
@@ -0,0 +1,18 @@
+package tictactoe
+
+fun main() {
+    val x = "X"
+    val o = "O"
+
+    print(x)
+    print(x)
+    print(o)
+   println()
+    print(x)
+    print(o)
+    print(o)
+    println()
+    print(o)
+    print(x)
+    print(x)
+}
